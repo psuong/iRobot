f194c94f59072a0dfce18aa6bfc472044d276548
MD Islam <mdislamwork@gmail.com>
2017-02-15 23:59:08 -0500

    Refactored writers

--- camera.py
+++ camera.py
@@ -4,6 +4,7 @@ http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_vide
 
 from datetime import datetime
 import os
+from abc import ABCMeta, abstractmethod, abstractproperty
 
 import numpy as np
 import cv2
@@ -17,51 +18,78 @@ class CameraError(Exception):
     pass
 
 
-class CameraWriter(object):
-    def __init__(self, output_name=None, fps=20, size=(640, 480), write_img=False):
+class Writer(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, fps=20, size=(640, 480)):
+        if not isinstance(size, tuple):
+            raise CameraError("Frame size parameter must be a tuple")
+
+        self.size = size
+        self.fps = fps
+        self.frames_recorded = 0
+
+    @abstractmethod
+    def write(self, frame):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
+
+    def __repr__(self):
+        return str(self.fps) + "fps" + " size: " + str(self.size)
+
+
+class VideoWriter(Writer):
+    def __init__(self, output_name=None, *args, **kwargs):
+        super().__init__()
+
         timestamp = datetime.now().strftime('%m-%d-%y_%H-%M-%S')
         self.output_name = output_name or "out_" + timestamp
         self.output_name = VIDEO_DIR + self.output_name + '.avi'
-        self.fps = fps
-        if not isinstance(size, tuple):
-            raise Camera("Frame size parameter must be a tuple")
-        self.size = size
 
         codec = cv2.VideoWriter_fourcc(*'DIVX')
-        self.writer = cv2.VideoWriter(output_name, codec, fps, size)
-
-        self.write_img = write_img
-        self.frames_recorded = 0
+        self.writer = cv2.VideoWriter(self.output_name,
+                                      codec,
+                                      self.fps,
+                                      self.size)
 
     def write(self, frame):
         self.writer.write(frame)
-
-        if self.write_img:
-            self.__write_img(frame)
-
         self.frames_recorded += 1
-
-    def __write_img(self, frame):
-        timestamp = datetime.now().strftime('%m-%d-%y_%H-%M-%S')
-        cv2.imwrite(IMG_DIR + timestamp + '.jpg', frame)
 
     def close(self):
         self.writer.release()
 
-    def __repr__(self):
-        return self.output_name + " @ " + str(self.fps) + "fps" + " size: " + str(self.size)
+
+class ImageWriter(Writer):
+
+
+    def write(self, frame):
+        timestamp = datetime.now().strftime('%m-%d-%y_%H-%M-%S')
+        cv2.imwrite(IMG_DIR + str(self.frames_recorded) + '_' + timestamp + '.jpg', frame)
+        self.frames_recorded += 1
+
+
+    def close(self):
+        pass
 
 
 class Camera(object):
-    def __init__(self, device=0, video=False, image=False, writer=None):
+    def __init__(self, device=0, video=False, image=False, video_writer=None,
+                 image_writer=None):
         self.device = device
         self.camera = cv2.VideoCapture(self.device)
         self.write = video or image
+        self.write_image = image
+        self.write_video = video
         if self.write:
             self.check_dirs()
-
-            self.writer = writer or CameraWriter(write_img=image)
-            print(str(self.writer))
+            if self.write_video:
+                self.video_writer = video_writer or VideoWriter()
+            if self.write_image:
+                self.image_writer = image_writer or ImageWriter()
 
     def check_dirs(self):
         if not os.path.exists(VIDEO_DIR):
@@ -74,8 +102,13 @@ class Camera(object):
         status, frame = self.camera.read()
         if status:
             frame = self.preprocess(frame)
+
             if self.write:
-                self.writer.write(cv2.flip(frame, 0))
+                if self.write_video:
+                    self.video_writer.write(cv2.flip(frame, 0))
+                if self.write_image:
+                    self.image_writer.write(frame)
+
             return frame
         else:
             raise CameraError("Cannot fetch frame")
@@ -87,8 +120,10 @@ class Camera(object):
     def close(self):
         if self.camera.isOpened():
             self.camera.release()
-        if self.write:
-            self.writer.close()
+        if self.write_video:
+            self.video_writer.close()
+        if self.write_image:
+            self.image_writer.close()
 
     def __del__(self):
         self.close()
@@ -96,7 +131,7 @@ class Camera(object):
 
 
 if __name__ == '__main__':
-    cam = Camera(image=True)
+    cam = Camera(image=True, video=True)
     for i in range(0, 30):
         print(i)
         cam.frame()
