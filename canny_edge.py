c0a98719fed97860a501416b5858bf2134048862
Porrith Suong <psuong95@gmail.com>
2017-02-15 22:47:53 -0500

    Adds todos for the functions.

--- canny_edge.py
+++ canny_edge.py
@@ -1,3 +1,13 @@
+"""
+Reference link: http://docs.opencv.org/trunk/da/d22/tutorial_py_canny.html
+"""
+
 import cv2
 import numpy
 from matplotlib import pyplot
+
+
+
+if __name__ == "__main__":
+    # TODO: Run the function which performs the canny edge detection
+    pass
