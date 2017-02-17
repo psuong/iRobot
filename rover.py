1179967f39d181e8a972c287e04b1d8363214d42
MD Islam <mdislamwork@gmail.com>
2017-02-16 22:17:30 -0500

    added basic datagram socket communication

---
+++ rover.py
@@ -0,0 +1,34 @@
+"""
+https://wiki.python.org/moin/UdpCommunication
+"""
+
+import socket
+
+
+UDP_IP = '192.168.2.3'
+UDP_PORT = 9000
+
+# Move bytecodes
+class Move(object):
+    BACKWARD = b'$5'
+    BACKWARD_LEFT = b'$7'
+    BACKWARD_RIGHT = b'$6'
+    FORWARD = b'$0'
+    FORWARD_LEFT = b'$3'
+    FORWARD_RIGHT = b'$4'
+    LEFT = b'$8'
+    RIGHT = b'$9'
+    KEEP_ALIVE = b'$?'
+
+
+class Rover(object):
+    def __init__(self, udp_ip=UDP_IP, udp_port=UDP_PORT):
+        self.udp_ip = udp_ip
+        self.udp_port = udp_port
+        self.udp_pair = (self.udp_ip, self.udp_port)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def send(self, message):
+        if not isinstance(message, bytes):
+            message = message.encode('utf-8')
+        self.sock.sendto(message, self.udp_pair)
