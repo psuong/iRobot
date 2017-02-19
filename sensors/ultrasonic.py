4d064957f88b8c45a7c04f3c1929ef1df184e43d
MD Islam <mdislamwork@gmail.com>
2017-02-19 16:14:30 -0500

    added ultrasonic sensor distance measurement

---
+++ sensors/ultrasonic.py
@@ -0,0 +1,44 @@
+# http://stackoverflow.com/questions/32300000/galileo-and-ultrasonic-error-when-distance-less-than-4cm
+import mraa
+import time
+
+trig = mraa.Gpio(3)
+echo = mraa.Gpio(4)
+
+trig.dir(mraa.DIR_OUT)
+echo.dir(mraa.DIR_IN)
+
+
+def distance(measure='cm'):
+    trig.write(0)
+    time.sleep(0.1)
+
+    trig.write(1)
+    time.sleep(0.00001)
+    trig.write(0)
+
+    while echo.read() == 0:
+            nosig = time.time()
+    sig = None
+    while echo.read() == 1:
+            sig = time.time()
+
+    if sig is None:
+        return -1
+    # et = Elapsed Time
+    et = sig - nosig
+
+    if measure == 'cm':
+            distance =  et * 17150
+    elif measure == 'in':
+            distance = et / 0.000148
+    else:
+            print('improper choice of measurement!!')
+            distance = None
+
+    return distance if distance < 80 else -1
+
+if __name__ == '__main__':
+    while True:
+        print(distance('cm'))
+        time.sleep(1)
