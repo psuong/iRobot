2130ad30f91690d4b6884fac7db186a3ed67d7fb
Porrith Suong <psuong95@gmail.com>
2017-03-08 12:30:20 -0500

    Updates the gitignore to handle the archives folder.

--- canny_edge.py
+++ canny_edge.py
@@ -33,14 +33,17 @@ class EdgeDetector(object):
         img = cv2.imread(image, 0)
         edges = cv2.Canny(img, threshold1, threshold2)
 
+        print (type(edges))
+
         plt.subplot(121), plt.imshow(img, cmap="gray")
         plt.title("Original Image"), plt.xticks([]), plt.yticks([])
         plt.subplot(122), plt.imshow(edges, cmap="gray")
         plt.title("Edged Image"), plt.xticks([]), plt.yticks([])
         plt.show()
 
-    def hough_transform(self, image: str):
-        
+    def hough_transform(self, raw_image: str, edged_image: str):
+        img = cv2.imread(image)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         pass
 
 
