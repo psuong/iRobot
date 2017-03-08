2589354c7bf961118e47234323ed9a02cc87516c
MD Islam <mdislamwork@gmail.com>
2017-03-08 17:14:16 -0500

    Added vagrant file and updated joystick module

--- sensors/joystick.py
+++ sensors/joystick.py
@@ -1,25 +1,22 @@
-from mraa import Aio
+"""
+Some sample mappings:
 
-from rover import RoverClient, Move
+x, y = vr_x.read(), vr_y.read()
+if y == 0 and 500 < x < 600:
+    rover.send(Move.FORWARD)
+elif y > 1000 and 500 < x < 600:
+    rover.send(Move.BACKWARD)
+elif x == 0 and 500 < y < 600:
+    rover.send(Move.LEFT)
+elif x > 1000 and 300 < y < 600:
+    rover.send(Move.RIGHT)
+else:
+    pass
+"""
+from mraa import Aio
 
 X_PIN = 0
 Y_PIN = 1
 
 vr_x = Aio(X_PIN)
 vr_y = Aio(Y_PIN)
-
-
-if __name__ == '__main__':
-    rover = RoverClient()
-    while True:
-        x, y = vr_x.read(), vr_y.read()
-        if y == 0 and 500 < x < 600:
-            rover.send(Move.FORWARD)
-        elif y > 1000 and 500 < x < 600:
-            rover.send(Move.BACKWARD)
-        elif x == 0 and 500 < y < 600:
-            rover.send(Move.LEFT)
-        elif x > 1000 and 300 < y < 600:
-            rover.send(Move.RIGHT)
-        else:
-            pass
