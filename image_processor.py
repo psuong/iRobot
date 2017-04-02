e3f8e4073bf72796b40ef11dc196fbf8924571b7
Sam <scohen000@citymail.cuny.edu>
2017-04-02 16:17:49 -0400

    Made the image processor output an image even if it loses sight of an image

--- image_processor.py
+++ image_processor.py
@@ -41,7 +41,7 @@ class ImageProcessor(object):
     def phough_transform(self, edged_image: np.ndarray, image: np.ndarray) -> np.ndarray:
         lines = cv2.HoughLinesP(edged_image, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
         if lines is None:
-            return None
+            return image
         for line in lines:
             x1, y1, x2, y2 = line[0][0], line[0][1], line[0][2], line[0][3]
             cv2.line(image, (x1, y1), (x2, y2), color=(66, 244, 69), thickness=6)
