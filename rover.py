ebd18f39682f8f17f8f8f488ed1398614310e470
exp0nge <mdislamwork@gmail.com>
2017-04-11 01:02:38 -0400

    Add new rover code

--- rover.py
+++ rover.py
@@ -1,38 +1,62 @@
 """
-https://wiki.python.org/moin/UdpCommunication
+Execute in Intel Edison
 """
 
-import socket
+import mraa
+import time
+
+DEFAULT_RUN_LENGTH = 0.3
 
 
-UDP_IP = '192.168.2.3'
-UDP_PORT = 9000
+class Rover(object):
+    def __init__(self, left=(0, 1), right=(3, 4)):
+        # GPIO pin setup
+        self.left_backward = mraa.Gpio(left[0])
+        self.left_forward = mraa.Gpio(left[1])
 
-# Move bytecodes
-class Move(object):
-    BACKWARD = b'$5'
-    BACKWARD_LEFT = b'$7'
-    BACKWARD_RIGHT = b'$6'
-    FORWARD = b'$0'
-    FORWARD_LEFT = b'$3'
-    FORWARD_RIGHT = b'$4'
-    LEFT = b'$8'
-    RIGHT = b'$9'
-    KEEP_ALIVE = b'$?'
-    POWER = b'$!'
+        self.right_forward = mraa.Gpio(right[2])
+        self.right_backward = mraa.Gpio(right[4])
 
-class RoverClient(object):
-    def __init__(self, udp_ip=UDP_IP, udp_port=UDP_PORT):
-        self.udp_ip = udp_ip
-        self.udp_port = udp_port
-        self.udp_pair = (self.udp_ip, self.udp_port)
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.left_forward.dir(mraa.DIR_OUT)
+        self.left_backward.dir(mraa.DIR_OUT)
+        self.right_backward.dir(mraa.DIR_OUT)
+        self.right_forward.dir(mraa.DIR_OUT)
 
-    def send(self, message):
-        # Send a Move bytecode
-        if not isinstance(message, bytes):
-            message = message.encode('utf-8')
-        self.sock.sendto(message, self.udp_pair)
+        self.halt()
 
-    def connect_wifi(self):
-        raise NotImplementedError()
+    def halt(self):
+        self.left_forward.write(0)
+        self.left_backward.write(0)
+        self.right_forward.write(0)
+        self.right_backward.write(0)
+
+    def on(self, *args):
+        for arg in args:
+            arg.write(1)
+
+    def off(self, *args):
+        for arg in args:
+            arg.write(1)
+
+    def nap(self, seconds=DEFAULT_RUN_LENGTH):
+        time.sleep(seconds)
+
+    def forward(self, seconds=DEFAULT_RUN_LENGTH):
+        self.on(self.right_forward, self.left_forward)
+        self.nap(seconds)
+        self.off(self.right_forward, self.left_forward)
+
+    def backward(self, seconds=DEFAULT_RUN_LENGTH):
+        self.on(self.right_backward, self.left_backward)
+        self.nap(seconds)
+        self.off(self.right_backward, self.left_backward)
+
+    def forward_right(self, seconds=DEFAULT_RUN_LENGTH):
+        self.on(self.left_forward)
+        self.nap(seconds)
+        self.off(self.left_forward)
+
+    def forward_left(self, seconds=DEFAULT_RUN_LENGTH):
+        self.on(self.right_forward)
+        self.nap(seconds)
+        self.off(self.right_forward)
