1def10ba7c180d7d314228bc27b063aa47637b1c
Sam <scohen000@citymail.cuny.edu>
2017-04-24 22:17:16 -0400

    Added detection of directional keys pressed

--- ML/collect_data.py
+++ ML/collect_data.py
@@ -8,7 +8,6 @@ class DataCollector(object):
 
     def __init__(self):
 
-        pygame.init()
         self.FORW = 0
         self.BACK = 1
         self.RIGHT = 2
@@ -16,6 +15,17 @@ class DataCollector(object):
         self.F_RIGHT = 4
         self.F_LEFT = 5
 
+
+    def test_pygame(self):
+        #Testing pygame
+        pygame.init()
+        pygame.display.set_mode()
+        while True:
+            for evt in pygame.event.get():
+                if evt.type == KEYDOWN:
+                    print("Help")
+                    keyinput = pygame.key.get_pressed()
+                    return
 
 
     def process_images(self, video):
@@ -28,14 +38,32 @@ class DataCollector(object):
         #might want to convert to storing results of lane detection
         imglist = []
         labels = []
-
+        pygame.init()
+        screen = pygame.display.set_mode((640, 480))
+        pygame.display.iconify()
         while video.isOpened():
             ret, frame = video.read()
-            cv2.imshow('', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if not ret:
                 break
+            screen.fill([0,0,0])
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = np.rot90(frame)
+            frame = pygame.surfarray.make_surface(frame)
+            screen.blit(frame, (0,0))
+            pygame.display.update()
+            pygame.time.wait(33) #30fps
+            keyinput = pygame.key.get_pressed()
 
-
+            if keyinput[pygame.K_UP]:
+                print("Forward")
+            elif keyinput[pygame.K_RIGHT]:
+                print("Right")
+            elif keyinput[pygame.K_LEFT]:
+                print("Left")
+            elif keyinput[pygame.K_DOWN]:
+                print("Back")
+            for e in pygame.event.get():
+                pass
 
 
 
