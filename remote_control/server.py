745081d573f266d1607bbb1433f041a539aca971
MD Islam <mdislamwork@gmail.com>
2017-04-26 15:04:40 -0400

    Add run in background script for socket server

---
+++ remote_control/server.py
@@ -0,0 +1,34 @@
+import socket
+
+from .common import UDP_IP_PORT, Keys
+import utility
+
+if utility.does_module_exists('mraa'):
+    import mraa
+    import rover
+    car = rover.RoverClient()
+    car_move = True
+else:
+    car_move = False
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(UDP_IP_PORT)
+
+def receive_loop():
+    while True:
+        data, addr = sock.recvfrom(1024)
+        if car_move:
+            if data == Keys.KEY_UP:
+                car.forward()
+            elif data == Keys.KEY_DOWN:
+                car.backward()
+            elif data == Keys.KEY_LEFT:
+                car.forward_left()
+            elif data == Keys.KEY_RIGHT:
+                car.forward_right()
+            elif data == Keys.KEY_SPACE:
+                car.halt()
+
+
+if __name__ == '__main__':
+    receive_loop()
---
+++ run_intel_socket_server.sh
@@ -0,0 +1,2 @@
+chmod +x remote_control/server.py
+nohup remote_control/server.py &
