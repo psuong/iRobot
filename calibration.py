ea448482e6825e4ace7e6691c357818e9fa300d2
Porrith Suong <psuong95@gmail.com>
2017-05-04 12:14:54 -0400

    Revamped the HSV color picker.

--- calibration.py
+++ calibration.py
@@ -1,159 +1,23 @@
-from __future__ import print_function
 import cv2
-from camera import VideoReader
-from image_processor import ImageProcessor
+from calibration_data import HSVData
 from imutils.video import WebcamVideoStream
-from imutils.video import FPS
-import argparse
-import imutils
-import numpy as np
+import argparse as ap
 
 
-def void_delegate(value: float):
-    pass
+def calibrate_hsv(src=0):
+    hsv_data = HSVData()
+    camera_stream = WebcamVideoStream(src=src).start()
 
-
-def calibrate_canny(video_path):
-    video_reader = VideoReader(video_path)
-    video_reader.open_video()
-
-    image_processor = ImageProcessor()
-
-    generator = video_reader.read_video()
-
-    for image, status in generator:
-        window_name = "Edged Image"
-        threshold1_name = "Threshold 1"
-        threshold2_name = "Threshold 2"
-        aperture_name = "Aperture Size"
-
-        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-
-        # Create the
-        cv2.createTrackbar(threshold1_name, window_name, image_processor.threshold_1, 1000, void_delegate)
-        cv2.createTrackbar(threshold2_name, window_name, image_processor.threshold_2, 2000, void_delegate)
-        cv2.createTrackbar(aperture_name, window_name, 3, 10, void_delegate)
-
-        # edged_image = image_processor.edge_detect(image)
-        while True:
-            cv2.imshow(window_name, edged_image)
-
-            image_processor.threshold_1 = cv2.getTrackbarPos(threshold1_name, window_name)
-            image_processor.threshold_2 = cv2.getTrackbarPos(threshold2_name, window_name)
-            image_processor.aperture_size = cv2.getTrackbarPos(aperture_name, window_name)
-
-            edged_image = cv2.Canny(image, image_processor.threshold_1, image_processor.threshold_2,
-                                    apertureSize=image_processor.aperture_size)
-
-            key = cv2.waitKey(33)
-            if key == 27:
-                break
-        cv2.destroyAllWindows()
-
-
-def calibrate_hsv():
-    hue1 = 0
-    saturation1 = 0
-    value1 = 0
-    hue2 = 255
-    saturation2 = 20
-    value2 = 255
-
-    video_reader = VideoReader(1)
-    video_reader.open_video()
-
-    # generator = video_reader.read_video()
-
-    while video_reader.video.isOpened():
-        ret, image = video_reader.video.read()
-
-        cached_image = image
-
-        cv2.namedWindow("", cv2.WINDOW_NORMAL)
-
-        cv2.createTrackbar("HUE 1", "", hue1, 255, void_delegate)
-        cv2.createTrackbar("SAT 1", "", saturation1, 255, void_delegate)
-        cv2.createTrackbar("VAL 1", "", value1, 255, void_delegate)
-
-        cv2.createTrackbar("HUE 2", "", hue2, 255, void_delegate)
-        cv2.createTrackbar("SAT 2", "", saturation2, 255, void_delegate)
-        cv2.createTrackbar("VAL 2", "", value2, 255, void_delegate)
-
-        while True:
-            hue1 = cv2.getTrackbarPos("HUE 1", "")
-            saturation1 = cv2.getTrackbarPos("SAT 1", "")
-            value1 = cv2.getTrackbarPos("VAL 1", "")
-
-            hsv1 = np.array([hue1, saturation1, value1])
-
-            hue2 = cv2.getTrackbarPos("HUE 2", "")
-            saturation2 = cv2.getTrackbarPos("SAT 2", "")
-            value2 = cv2.getTrackbarPos("VAL 2", "")
-
-            hsv2 = np.array([hue2, saturation2, value2])
-
-            image = ImageProcessor.filter_colors(cached_image, hsv1, hsv2)
-            cv2.imshow("", image)
-            key = cv2.waitKey(33)
-            if key == 27:
-                break
-
-    cv2.destroyAllWindows()
-
-
-def main_multi_threaded():
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("-n", "--num-frames", type=int, default=100)
-    arg_parser.add_argument("-d", "--display", type=int, default=-1)
-    args = vars(arg_parser.parse_args())
-
-    camera_stream = WebcamVideoStream(src=0).start()
-    fps = FPS().start()
-
-    while fps._numFrames < args["num_frames"]:
+    while True:
         frame = camera_stream.read()
-        frame = imutils.resize(frame, width=400)
 
-        if args["display"] > 0:
-            cv2.imshow("", frame)
-            cv2.waitKey(1) & 0xFF
-
-        fps.update()
-
-    cv2.destroyAllWindows()
-    camera_stream.stop()
-    fps.stop()
-    print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-
-def main():
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("-n", "--num-frames", type=int, default=100)
-    arg_parser.add_argument("-d", "--display", type=int, default=-1)
-    args = vars(arg_parser.parse_args())
-
-    camera_stream = VideoReader(0).open_video()
-    fps = FPS().start()
-
-    while fps._numFrames < args["num_frames"]:
-        ret, frame = camera_stream.read()
-
-        frame = imutils.resize(frame, width=400)
-
-        if args["display"] > 0:
-            cv2.imshow("", frame)
-            cv2.waitKey(1) & 0xFF
-
-        fps.update()
-
-    cv2.destroyAllWindows()
-    fps.stop()
-    print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+        hsv_data.image = frame
+        hsv_data.define_hsv_range()
 
 
 if __name__ == "__main__":
-    # main()
-    # main_multi_threaded()
-    calibrate_hsv()
+    arg_parser = ap.ArgumentParser()
+    arg_parser.add_argument("-s", "--source", type=int, default=0, help="Source of the camera stream,"
+                                                                        "by default it's the default webcam")
+    args = vars(arg_parser.parse_args())
+    calibrate_hsv(args["source"])
--- calibration_data.py
+++ calibration_data.py
@@ -20,7 +20,10 @@ class HSVData(object):
     
     Convenience functions will exist to show the data
     """
-    def __init__(self, image):
+    def __init__(self):
+        # Represents the max range between 0 and the defined value
+        self._max_range = 255
+
         self.lower_bound = np.array([0, 0, 0])
         self.upper_bound = np.array([0, 0, 0])
         # Data range 1 (lower bound)
@@ -28,28 +31,26 @@ class HSVData(object):
         self.saturation_1 = 0
         self.value_1 = 0
         # Data range 2 (upper bound)
-        self.hue_2 = 0
-        self.saturation_2 = 0
-        self.value_2 = 0
+        self.hue_2 = self._max_range
+        self.saturation_2 = self._max_range
+        self.value_2 = self._max_range
         # Image data
-        self.image = self.image
-        # Represents the max range between 0 and the defined value
-        self._max_range = 255
+        self.image = None
+
 
     def define_hsv_range(self):
         # Perform the manipulation on the cached image, keep the original image intact
         cached_image = self.image
-
         window_name = "HSV_Calibration"
-
         cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-        cv2.createTrackbar("Hue 1", window_name, self.hue_1, self._max_range, void_delegate)
-        cv2.createTrackbar("Saturation 1", window_name, self.saturation_1, self._max_range, void_delegate)
-        cv2.createTrackbar("Value 1", window_name, self.value_1, self._max_range, void_delegate)
 
         cv2.createTrackbar("Hue 1", window_name, self.hue_1, self._max_range, void_delegate)
         cv2.createTrackbar("Saturation 1", window_name, self.saturation_1, self._max_range, void_delegate)
         cv2.createTrackbar("Value 1", window_name, self.value_1, self._max_range, void_delegate)
+
+        cv2.createTrackbar("Hue 2", window_name, self.hue_2, self._max_range, void_delegate)
+        cv2.createTrackbar("Saturation 2", window_name, self.saturation_2, self._max_range, void_delegate)
+        cv2.createTrackbar("Value 2", window_name, self.value_2, self._max_range, void_delegate)
 
         while True:
             self.hue_1 = cv2.getTrackbarPos("Hue 1", window_name)
